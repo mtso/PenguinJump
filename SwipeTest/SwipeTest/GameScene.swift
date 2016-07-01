85db1506b7d90ef1e44a9775be89f06331c59338
mtso <mtsobot@gmail.com>
2016-07-01 11:52:46 -0700

    Add ability to save the double jump during first move in the first jump and keep ability to invoke double jump during second move of first jump

--- SwipeTest/SwipeTest/GameScene.swift
+++ SwipeTest/SwipeTest/GameScene.swift
@@ -16,9 +16,10 @@ class GameScene: SKScene {
     var player: SKSpriteNode! //(color: UIColor.blackColor(), size: CGSize(width: 100, height: 100) )
     var cam: SKCameraNode!
     
-    let numberOfJumps = 2
+    let maxJumps = 2
     var jumpCount = 0
     var okToJump = true
+    var useSavedJump = false
     
     var swipeTrail: SKEmitterNode!
     
@@ -117,29 +118,18 @@ class GameScene: SKScene {
     }
     
     func jump(deltaX: CGFloat, _ deltaY: CGFloat) {
-        if jumpCount < numberOfJumps && okToJump {
+        
+        if okToJump {
             jumpCount += 1
             okToJump = false
             
             player.removeAllActions()
-            for child in children {
-                if child.name == "target" {
-                    child.removeFromParent()
-                }
-            }
             
-            let target = SKSpriteNode(imageNamed: "targetcircle")
-            target.name = "target"
-            target.position = CGPoint(x: player.position.x - deltaX, y: player.position.y - deltaY)
-            target.setScale(0.5)
-            target.zPosition = 100
-            addChild(target)
+            childNodeWithName("target")?.removeFromParent()
+            let jumpDestination = CGPoint(x: player.position.x - deltaX, y: player.position.y - deltaY)
+            addChild( newTargetAt( jumpDestination ) )
             
-            let flashDown = SKAction.fadeAlphaTo(0, duration: 0.1)
-            let flashUp = SKAction.fadeAlphaTo(1, duration: 0.1)
-            let wait = SKAction.waitForDuration(0.1)
-            target.runAction(SKAction.repeatActionForever(SKAction.sequence([flashUp, flashDown, wait])))
-            
+            // Movement and scale `SKActions`
             let scaleUp = SKAction.scaleTo(1.5, duration: 0.5)
             scaleUp.timingMode = .EaseOut
             let scaleDown = SKAction.scaleTo(1, duration: 0.3)
@@ -147,15 +137,98 @@ class GameScene: SKScene {
             
             let moveFirst = SKAction.moveBy(CGVector(dx: -deltaX * 0.9, dy: -deltaY * 0.9), duration: 0.5)
             let moveSecond = SKAction.moveBy(CGVector(dx: -deltaX * 0.1, dy: -deltaY * 0.1), duration: 0.3)
-            //move.timingMode = .EaseOut
+            moveFirst.timingMode = .EaseOut
             
             player.runAction(SKAction.group([moveFirst, scaleUp]), completion: {
+                
+                if self.useSavedJump {
+                    
+                    self.secondJump(nil, nil, saveDelta: false)
+                    
+                } else if self.jumpCount < self.maxJumps {
+                    
+                    // If double jump during landing, set `okToJump` to true before completion
+                    self.okToJump = true
+
+                    self.player.runAction(SKAction.group([moveSecond, scaleDown]), completion: {
+                        self.childNodeWithName("target")?.removeFromParent()
+                        self.jumpCount = 0
+                    })
+                } else {
+                    
+                    self.player.runAction(SKAction.group([moveSecond, scaleDown]), completion: {
+                        
+                        // If already double jumped once, set `okToJump` to true after completion
+                        self.okToJump = true
+
+                        self.childNodeWithName("target")?.removeFromParent()
+                        self.jumpCount = 0
+                    })
+                }
+            })
+            
+        } else if jumpCount < maxJumps {
+            jumpCount += 1
+            
+            secondJump(deltaX, deltaY, saveDelta: true)
+            useSavedJump = true
+        }
+    }
+    
+    func secondJump(deltaX: CGFloat?, _ deltaY: CGFloat?, saveDelta saving: Bool) {
+        
+        struct SavedDelta {
+            static var deltaX: CGFloat = 0
+            static var deltaY: CGFloat = 0
+        }
+        
+        if saving {
+            if let dx = deltaX { SavedDelta.deltaX = dx }
+            if let dy = deltaY { SavedDelta.deltaY = dy }
+        } else {
+            player.removeAllActions()
+            childNodeWithName("target")?.removeFromParent()
+            
+            let deltaX2 = SavedDelta.deltaX
+            let deltaY2 = SavedDelta.deltaY
+            
+            let jumpDestination = CGPoint(x: player.position.x - deltaX2, y: player.position.y - deltaY2)
+            addChild( newTargetAt( jumpDestination ) )
+            
+            // Movement and scale `SKActions`
+            let scaleUp = SKAction.scaleTo(1.5, duration: 0.5)
+            let scaleDown = SKAction.scaleTo(1, duration: 0.3)
+            scaleUp.timingMode = .EaseOut
+            scaleDown.timingMode = .EaseIn
+            let moveFirst = SKAction.moveBy(CGVector(dx: -deltaX2 * 0.9, dy: -deltaY2 * 0.9), duration: 0.5)
+            let moveSecond = SKAction.moveBy(CGVector(dx: -deltaX2 * 0.1, dy: -deltaY2 * 0.1), duration: 0.3)
+            moveFirst.timingMode = .EaseOut
+            
+            let scale = SKAction.sequence([scaleUp, scaleDown])
+            let move = SKAction.sequence([moveFirst, moveSecond])
+            
+            player.runAction(SKAction.group([scale, move]), completion: {
+                self.childNodeWithName("target")?.removeFromParent()
                 self.okToJump = true
-                self.player.runAction(SKAction.group([moveSecond, scaleDown]), completion: {
-                    target.removeFromParent()
-                    self.jumpCount = 0
-                })
+                self.jumpCount = 0
+                self.useSavedJump = false
             })
         }
+    }
+    
+    func newTargetAt(position: CGPoint) -> SKSpriteNode {
+        
+        let target = SKSpriteNode(imageNamed: "targetcircle")
+        target.name = "target"
+        target.position = position // CGPoint(x: player.position.x - deltaX, y: player.position.y - deltaY)
+        target.setScale(0.5)
+        target.zPosition = 100
+        
+        let flashDown = SKAction.fadeAlphaTo(0, duration: 0.1)
+        let flashUp = SKAction.fadeAlphaTo(1, duration: 0.1)
+        let wait = SKAction.waitForDuration(0.1)
+        target.runAction(SKAction.repeatActionForever(SKAction.sequence([flashUp, flashDown, wait])))
+        
+        return target
     }
 }
